ced0017a313e1fa9903ac9dfba06f485512ccfa7
Adarsh Kesarwani <adarsh.08122003@gmail.com>
2026-03-16 23:46:31 +0530

    Added sequelize congfig for migrations

---
+++ HotelService/src/config/db.config.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const Config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  }
+};
+
+module.exports = Config;
+
+
---
+++ HotelService/src/config/sequelize.config.js
@@ -0,0 +1,3 @@
+require('ts-node/register'); //this line is required to run .ts files in sequelize-cli
+const config = require('./db.config');
+module.exports = config;
